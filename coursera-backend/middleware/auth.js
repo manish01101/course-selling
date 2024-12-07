42449fa1acf295a80a3a7f0eb008d2bdbe04c826
manish01101 <manishchotu4@gmail.com>
2024-12-07 18:36:04 +0530

    used try catch

--- coursera-backend/middleware/auth.js
+++ coursera-backend/middleware/auth.js
@@ -2,18 +2,22 @@ const jwt = require("jsonwebtoken");
 const SECRET = process.env.SECRET;
 
 const authenticateJwt = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (authHeader) {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({ message: "Authorization header missing" });
+    }
+
     const token = authHeader.split(" ")[1];
     jwt.verify(token, SECRET, (err, user) => {
       if (err) {
-        return res.sendStatus(403);
+        return res.status(403).json({ message: "Invalid or expired token" });
       }
       req.user = user;
       next();
     });
-  } else {
-    res.sendStatus(401);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
