da366becd72749d20b8584e30b43db8c7b48f81a
manish01101 <manishchotu4@gmail.com>
2024-12-08 08:15:35 +0530

    added middleware for user and admin separately

--- coursera-backend/middleware/auth.js
+++ coursera-backend/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
-const SECRET = process.env.SECRET;
+const { USER_SECRET, ADMIN_SECRET } = require("../config");
 
-const authenticateJwt = (req, res, next) => {
+const authenticateUser = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
@@ -9,7 +9,27 @@ const authenticateJwt = (req, res, next) => {
     }
 
     const token = authHeader.split(" ")[1];
-    jwt.verify(token, SECRET, (err, user) => {
+    jwt.verify(token, USER_SECRET, (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: "Invalid or expired token" });
+      }
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const authenticateAdmin = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({ message: "Authorization header missing" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, ADMIN_SECRET, (err, user) => {
       if (err) {
         return res.status(403).json({ message: "Invalid or expired token" });
       }
@@ -22,6 +42,6 @@ const authenticateJwt = (req, res, next) => {
 };
 
 module.exports = {
-  authenticateJwt,
-  SECRET,
+  authenticateAdmin,
+  authenticateUser,
 };
