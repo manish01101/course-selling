71fd2a2419a7f09ce90e06b7d66d2b4beac97aa1
manish01101 <manishchotu4@gmail.com>
2024-12-10 09:46:33 +0530

    updated and corrected user routes

--- coursera-backend/routes/user.js
+++ coursera-backend/routes/user.js
@@ -1,61 +1,123 @@
-const express = require('express');
-const { authenticateJwt, SECRET } = require("../middleware/auth");
-const { User, Course, Admin } = require("../db");
-const router = express.Router();
+import { Router } from "express";
+import { authenticateUser } from "../middleware/auth";
+import { Course, User } from "../db";
+import { userSchemaType } from "../types";
+import jwt from "jsonwebtoken";
+import { USER_SECRET } from "../config";
 
-  router.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if (user) {
-      res.status(403).json({ message: 'User already exists' });
-    } else {
-      const newUser = new User({ username, password });
-      await newUser.save();
-      const token = jwt.sign({ username, role: 'user' }, SECRET, { expiresIn: '1h' });
-      res.json({ message: 'User created successfully', token });
+const userRouter = Router();
+
+userRouter.get("/me", authenticateUser, async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.user.username });
+    if (!user) {
+      return res.status(403).json({ msg: "User does not exist" });
     }
-  });
-  
-  router.post('/signin', async (req, res) => {
-    const { username, password } = req.headers;
+    res.json({
+      username: user.username,
+    });
+  } catch (error) {
+    console.error("Error during fetching user profile", error);
+    return res
+      .status(500)
+      .json({ message: "Error during fetching user profile" });
+  }
+});
+
+userRouter.post("/signup", async (req, res) => {
+  const parsedBody = userSchemaType.safeParse(req.body);
+  if (!parsedBody.success) {
+    return res
+      .status(400)
+      .json({ message: "Invalid input data", errors: parsedBody.error.errors });
+  }
+
+  const { username, password } = parsedBody.data;
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(403).json({ message: "User already exists" });
+    }
+    const newUser = new User({ username, password });
+    await newUser.save();
+
+    const token = jwt.sign({ username, role: "user" }, USER_SECRET, {
+      expiresIn: "24h",
+    });
+    return res.json({ message: "User created successfully", token });
+  } catch (error) {
+    console.error("Error during user signup", error);
+    return res.status(500).json({ message: "Error during user signup" });
+  }
+});
+
+userRouter.post("/signin", async (req, res) => {
+  const parsedBody = userSchemaType.safeParse(req.body);
+  if (!parsedBody.success) {
+    return res
+      .status(400)
+      .json({ message: "Invalid input data", errors: parsedBody.error.errors });
+  }
+
+  const { username, password } = parsedBody.data;
+  try {
     const user = await User.findOne({ username, password });
     if (user) {
-      const token = jwt.sign({ username, role: 'user' }, SECRET, { expiresIn: '1h' });
-      res.json({ message: 'Logged in successfully', token });
+      const token = jwt.sign({ username, role: "user" }, USER_SECRET, {
+        expiresIn: "24h",
+      });
+      res.json({ message: "Logged in successfully", token });
     } else {
-      res.status(403).json({ message: 'Invalid username or password' });
+      return res.status(401).json({ message: "Invalid username or password" });
     }
-  });
-  
-  router.get('/courses', authenticateJwt, async (req, res) => {
-    const courses = await Course.find({published: true});
+  } catch (error) {
+    console.error("Error during user signin", error);
+    return res.status(500).json({ message: "Error during user signin" });
+  }
+});
+
+userRouter.get("/courses", authenticateUser, async (req, res) => {
+  try {
+    const courses = await Course.find({ published: true });
     res.json({ courses });
-  });
-  
-  router.post('/courses/:courseId', authenticateJwt, async (req, res) => {
+  } catch (error) {
+    console.error("Error during fetching courses", error);
+    return res.status(500).json({ message: "Error during fetching courses:" });
+  }
+});
+
+userRouter.post("/courses/:courseId", authenticateUser, async (req, res) => {
+  try {
     const course = await Course.findById(req.params.courseId);
-    console.log(course);
-    if (course) {
-      const user = await User.findOne({ username: req.user.username });
-      if (user) {
-        user.purchasedCourses.push(course);
-        await user.save();
-        res.json({ message: 'Course purchased successfully' });
-      } else {
-        res.status(403).json({ message: 'User not found' });
-      }
-    } else {
-      res.status(404).json({ message: 'Course not found' });
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
     }
-  });
-  
-  router.get('/purchasedCourses', authenticateJwt, async (req, res) => {
-    const user = await User.findOne({ username: req.user.username }).populate('purchasedCourses');
-    if (user) {
-      res.json({ purchasedCourses: user.purchasedCourses || [] });
-    } else {
-      res.status(403).json({ message: 'User not found' });
+    const user = await User.findOne({ username: req.user.username });
+    if (!user) {
+      res.status(403).json({ message: "User not found" });
     }
-  });
-  
-  module.exports = router
+    user.purchasedCourses.push(course);
+    await user.save();
+    res.json({ message: "Course purchased successfully" });
+  } catch (error) {
+    console.error("Error during purchasing course", error);
+    return res.status(500).json({ message: "Error during purchasing course" });
+  }
+});
+
+userRouter.get("/purchasedCourses", authenticateUser, async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.user.username }).populate(
+      "purchasedCourses"
+    );
+    if (!user) {
+      return res.status(403).json({ message: "User not found" });
+    }
+    res.json({ purchasedCourses: user.purchasedCourses || [] });
+  } catch (error) {
+    console.error("Error during fetching courses", error);
+    return res.status(500).json({ message: "Error during fetching courses" });
+  }
+});
+
+export default userRouter;
